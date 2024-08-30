51813b1b8864cbb8f1052d791e2deeb8ee5d8201
Ivo Maceira <ivomaceira@gmail.com>
2024-08-30 15:38:53 +0200

    increase terminal split size

--- lua/plugin/toggleterm.lua
+++ lua/plugin/toggleterm.lua
@@ -7,7 +7,7 @@ return {
 				if term.direction == "horizontal" then
 					return 16
 				elseif term.direction == "vertical" then
-					return vim.o.columns * 0.33
+					return vim.o.columns * 0.4
 				end
 			end,
 			open_mapping = [[<c-\>]],
